7794b143fa8ab57f1afddba35416d5be8e1c4bdb
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-30 17:30:38 +0200

    Initial Commit

---
+++ SwiftUIGridsLazyVGridLazyHGridStaticGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIGridsLazyVGridLazyHGridStaticGrid
+//
+//  Created by Ramill Ibragimov on 3/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIGridsLazyVGridLazyHGridStaticGrid/SwiftUIGridsLazyVGridLazyHGridStaticGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGridsLazyVGridLazyHGridStaticGridApp.swift
+//  SwiftUIGridsLazyVGridLazyHGridStaticGrid
+//
+//  Created by Ramill Ibragimov on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridsLazyVGridLazyHGridStaticGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
