54d1ad537c89dcb8061d110bbc957abfdf109f5f
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-30 17:58:56 +0200

    pinnedView

--- SwiftUIGridsLazyVGridLazyHGridStaticGrid/ContentView.swift
+++ SwiftUIGridsLazyVGridLazyHGridStaticGrid/ContentView.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorHex = ""
+    var columns = Array(repeating: GridItem(.flexible()), count: 3)
+    
     var body: some View {
-        LazyVGrid(columns: [GridItem(.flexible())]) {
-            ForEach(MockData.colors, id: \.self) {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill($0)
-                    .frame(height: 100)
+        ScrollView {
+            LazyVGrid(columns: columns, pinnedViews: .sectionHeaders) {
+                Section {
+                    ForEach(MockData.colors, id: \.self) { color in
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(color)
+                            .frame(height: 100)
+                            .onTapGesture {
+                                colorHex = "\(color)"
+                            }
+                    }
+                } header: {
+                    Text("Home \(colorHex)")
+                        .font(.largeTitle.bold())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                
+                Section {
+                    ForEach(MockData.colors, id: \.self) { color in
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(color)
+                            .frame(height: 100)
+                            .onTapGesture {
+                                colorHex = "\(color)"
+                            }
+                    }
+                } header: {
+                    Text("Favorites \(colorHex)")
+                        .font(.largeTitle.bold())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
             }
+            .padding()
         }
     }
 }
